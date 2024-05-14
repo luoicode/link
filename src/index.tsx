13471c1be6472640d9661227683bf1ddedfa0cab
luoicode <162140145+luoicode@users.noreply.github.com>
2024-05-14 22:46:23 +0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <h1>Hello World~~</h1>
+            <a href=""></a>
+        </div>
+    )
+}
+
+export default index
